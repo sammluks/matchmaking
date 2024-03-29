a5a62f4b10e5402cda94d808b7c7bb2063c2ed35
Samuel Moraes <samlucas2608@gmail.com>
2024-03-29 11:22:07 -0300

    Tratando mais Exceptions

--- src/main/java/com/matchmaking/matchmaking/infra/exception/ExceptionsHandler.java
+++ src/main/java/com/matchmaking/matchmaking/infra/exception/ExceptionsHandler.java
@@ -1,6 +1,11 @@
 package com.matchmaking.matchmaking.infra.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,4 +28,28 @@ public class ExceptionsHandler {
         return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorDto::new).toList());
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> error400Handler(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+    
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> badCredentialsErrorHandler() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas!");
+    }
+    
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> authenticationExceptionHandler() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação!");
+    }
+    
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> accessDeniedExceptionHandler() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado!");
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> error500Handler(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + e.getLocalizedMessage() + "!");
+    }
 }
